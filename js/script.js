56ca342c6383bf3cab307baa72722a7f2b09fd84
Flavius Draghici <draghici.nic.flavius@gmail.com>
2026-02-17 18:15:48 +0200

    19

--- js/script.js
+++ js/script.js
@@ -21,3 +21,26 @@ function showPage(pageId) {
     // 3. Închide meniul
     toggleMenu();
 }
+const desertData = [
+    { name: "Cheesecake cu fructe", img: "images/cheesecake.jpg", price: "18.00 LEI" },
+    { name: "Lava Cake", img: "images/lava-cake.jpg", price: "22.00 LEI" },
+    { name: "Tiramisu", img: "images/tiramisu.jpg", price: "20.00 LEI" }
+];
+
+let currentDesertIndex = 0;
+
+function changeDesert(direction) {
+    currentDesertIndex += direction;
+
+    if (currentDesertIndex >= desertData.length) currentDesertIndex = 0;
+    if (currentDesertIndex < 0) currentDesertIndex = desertData.length - 1;
+
+    const nameElement = document.getElementById('desert-name');
+    const imgElement = document.getElementById('desert-img');
+    const priceElement = document.getElementById('desert-price');
+
+    // Actualizarea sincronizată a tuturor elementelor
+    nameElement.innerText = desertData[currentDesertIndex].name;
+    imgElement.src = desertData[currentDesertIndex].img;
+    priceElement.innerText = desertData[currentDesertIndex].price;
+}
