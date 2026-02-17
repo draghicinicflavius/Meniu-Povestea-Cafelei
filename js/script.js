654ded3278623f04f23773a6e983fe2dd148175b
Flavius Draghici <draghici.nic.flavius@gmail.com>
2026-02-17 18:35:04 +0200

    20

--- js/script.js
+++ js/script.js
@@ -22,25 +22,55 @@ function showPage(pageId) {
     toggleMenu();
 }
 const desertData = [
-    { name: "Cheesecake cu fructe", img: "images/cheesecake.jpg", price: "18.00 LEI" },
-    { name: "Lava Cake", img: "images/lava-cake.jpg", price: "22.00 LEI" },
-    { name: "Tiramisu", img: "images/tiramisu.jpg", price: "20.00 LEI" }
+    { 
+        name: "PISTACHIO DOME", 
+        img: "images/pistachio.jpg", 
+        price: "20.00 LEI",
+        weight: "100 g",
+        desc: "mousse de fistic pe blat de vanilie cu sos de fructe de padure"
+    },
+    { 
+        name: "TIRAMISU", 
+        img: "images/tiramisu.jpg", 
+        price: "20.00 LEI",
+        weight: "120 g",
+        desc: "faimosul desert italian cu crema fina de mascarpone si espresso"
+    },
+    { 
+        name: "LAVA CAKE", 
+        img: "images/lava-cake.jpg", 
+        price: "20.00 LEI",
+        weight: "120 g",
+        desc: "blat dens de cacao cu inima fierbinte de ciocolata"
+    },
+    { 
+        name: "CHEESECAKE", 
+        img: "images/cheesecake.jpg", 
+        price: "20.00 LEI",
+        weight: "135 g",
+        desc: "un tort rezistibil cu textura fina de branza si blat de biscuiti"
+    },
+    { 
+        name: "SORBET DE LĂMÂIE", 
+        img: "images/lemon-sorbet.jpg", 
+        price: "25.00 LEI",
+        weight: "90 g",
+        desc: "sorbet revigorant de lamaie servit in coaja fructului"
+    }
 ];
 
 let currentDesertIndex = 0;
 
 function changeDesert(direction) {
     currentDesertIndex += direction;
-
     if (currentDesertIndex >= desertData.length) currentDesertIndex = 0;
     if (currentDesertIndex < 0) currentDesertIndex = desertData.length - 1;
 
-    const nameElement = document.getElementById('desert-name');
-    const imgElement = document.getElementById('desert-img');
-    const priceElement = document.getElementById('desert-price');
-
-    // Actualizarea sincronizată a tuturor elementelor
-    nameElement.innerText = desertData[currentDesertIndex].name;
-    imgElement.src = desertData[currentDesertIndex].img;
-    priceElement.innerText = desertData[currentDesertIndex].price;
+    const current = desertData[currentDesertIndex];
+    
+    document.getElementById('desert-name').innerText = current.name;
+    document.getElementById('desert-img').src = current.img;
+    document.getElementById('desert-price').innerText = current.price;
+    document.getElementById('desert-weight').innerText = current.weight;
+    document.getElementById('desert-desc').innerText = current.desc;
 }
